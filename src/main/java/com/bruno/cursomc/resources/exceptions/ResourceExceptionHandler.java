873f4d9815262ad67371af73472977ad07cd3e5b
Bruno <brunoabneves97@gmail.com>
2019-09-15 23:16:07 -0300

    Personalizando o retorno da validação

--- src/main/java/com/bruno/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/bruno/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,12 +26,23 @@ public class ResourceExceptionHandler {
 	}
 	
 	//tratador de excessões do tipo Handler. Erro personalizado
-		@ExceptionHandler(DataIntegrityException.class)
-		public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 			
-			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 			
-		}
+	}
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+			
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		//Percorrendo a lista de erros da excessão "e" e, para cada erro da lista será gerado o objeto FieldMessage
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+			
+	}
 }
