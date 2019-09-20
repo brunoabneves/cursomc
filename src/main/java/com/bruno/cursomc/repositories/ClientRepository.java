99e826677320a4e5f673f51cc3dde21fbcc0ad98
Bruno <brunoabneves97@gmail.com>
2019-09-19 22:15:24 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/bruno/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/bruno/cursomc/repositories/ClientRepository.java
@@ -2,11 +2,17 @@ package com.bruno.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bruno.cursomc.domain.Client;
 
 //faz esta classe ter acesso à tabela "Categories" no BD
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer>{
-
+	
+	//O readOnly faz com que ela não nescessite ser envolvida como uma transação de BD
+	@Transactional(readOnly = true)
+	//Busca no bando de dados um cliente passando um email como argumento
+	Client findByEmail(String email);
+	
 }
--- src/main/java/com/bruno/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/bruno/cursomc/services/validation/ClientInsertValidator.java
@@ -8,11 +8,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bruno.cursomc.domain.Client;
 import com.bruno.cursomc.domain.enums.ClientType;
 import com.bruno.cursomc.dto.ClientNewDTO;
+import com.bruno.cursomc.repositories.ClientRepository;
 import com.bruno.cursomc.resources.exceptions.FieldMessage;
 import com.bruno.cursomc.services.validation.utils.BR;
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository repo;
+	
 	@Override
 	public void initialize(ClientInsert ann) {
 	
@@ -29,6 +37,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		
 		if (objDto.getType().equals(ClientType.LEGALPERSON.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrcnpj())) {
 			list.add(new FieldMessage("cpfOrcnpj", "CNPJ inválido"));
+		}
+		
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		/*Para cada objeto da lista do tipo FieldMessage (Erro personalizado),
