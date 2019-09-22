80308ed7a40b551e775c3e6eb4fcfb40dd9f1984
Bruno <brunoabneves97@gmail.com>
2019-09-22 15:37:26 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/bruno/cursomc/config/DevConfig.java
@@ -0,0 +1,38 @@
+//Classe de configuração específica do profile de dev
+package com.bruno.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bruno.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	//pega o valor da chave do arquivo properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		/*Instancia os dados apenas se o valor da chave for "create"
+		 * Evita a duplicação de dados
+		 */
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
