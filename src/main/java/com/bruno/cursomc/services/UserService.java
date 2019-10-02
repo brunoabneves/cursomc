b5169443e3e3b7325d8578a958f64211ffc0fa3c
Bruno <brunoabneves97@gmail.com>
2019-10-02 11:17:46 -0300

    Refresh token

--- src/main/java/com/bruno/cursomc/services/UserService.java
+++ src/main/java/com/bruno/cursomc/services/UserService.java
@@ -7,7 +7,7 @@ import com.bruno.cursomc.security.UserSS;
 public class UserService {
 	
 	//retorna o user logado no sistema
-	protected static UserSS authenticated() {
+	public static UserSS authenticated() {
 		try {
 			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		}
